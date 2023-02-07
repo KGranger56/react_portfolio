b63795e1284544ecb644f4ea56bb69c3b7a6ebad
KGranger56 <kelly.granger56@gmail.com>
2023-02-07 16:31:40 -0600

    Contact page updated

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -5,14 +5,9 @@ const Contact = () => {
   return (
     <div className="page contact">
       <h1>Contact Me</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-        officia deserunt mollit anim id est laborum.
-      </p>
+      <br/>
+      <h4>Thank you for taking the time to visit my page. <br/>I look forward to hearing from you soon.</h4>
+    <h4>kelly.granger56@gmail.com</h4>
     </div>
   );
 };
